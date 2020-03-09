0ca1b602f30a76b30a713d7456ebe95ee11159d0
759729997 <qiaoyongtian@qq.com>
2020-03-09 16:00:42 +0800

    数据流上传图片

--- scripts/tornado/file_send.py
+++ scripts/tornado/file_send.py
@@ -7,7 +7,16 @@ if __name__ == "__main__":
     """文件发送
     http://2.python-requests.org/zh_CN/latest/user/quickstart.html
     """
-    url = 'http://127.0.0.1:18000/upload'
-    files = {'file': codecs.open('./data/yule_head_10.csv', mode='r', encoding='utf8')}
-    r = requests.post(url, files=files)
-    print(r.text)
+    # json形式发送文件
+    # url = 'http://127.0.0.1:18000/upload'
+    # files = {'file': codecs.open('./data/yule_head_10.csv', mode='r', encoding='utf8')}
+    # r = requests.post(url, files=files)
+    # print(r.text)
+    # 二进制流发送文件；https://tool.oschina.net/commons/
+    url = "http://127.0.0.1:18000/upload_b"
+    payload = codecs.open('./data/jay.jpg', mode='rb')
+    headers = {
+        'Content-Type': 'image/jpeg'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text.encode('utf8'))
--- scripts/tornado/file_upload.py
+++ scripts/tornado/file_upload.py
@@ -18,13 +18,38 @@ class MainHandler(tornado.web.RequestHandler):
 class UploadHandler(tornado.web.RequestHandler):
     def post(self):
         files = self.request.files["file"]  # 此处的"file"对应HTML的name="file"
+        response_data = {'code': 0, 'message': 'ok'}
         for file in files:
             filename = file["filename"]
             print(filename)
             with open('./temp/' + filename, 'wb') as f:
                 # 文件内容保存 到'/temp/{filename}'
                 f.write(file['body'])
-        self.write("<p>{}上传成功!</p>".format(filename))
+                response_data['data'] = '{}上传成功!'.format(filename)
+        self.finish(response_data)
+
+
+class UploadBinaryHandler(tornado.web.RequestHandler):
+    """二进制流文件"""
+    def post(self):
+        response_data = {'code': 0, 'message': 'ok'}
+        header = self.request.headers
+        content_type = header.get('Content-Type', None)
+        print('content_type:{}'.format(content_type))
+        if content_type.startswith('image'):  # 图片
+            filename = 'aa_b.jpg'
+        else:
+            response_data['code'] = 101
+            response_data['message'] = 'Content-Type error'
+            response_data['data'] = '请选择图片进行上传'
+            self.finish(response_data)
+            return None
+        print(filename)
+        file = self.request.body
+        with open('./temp/' + filename, 'wb') as f:
+            f.write(file)
+        response_data['data'] = "{}上传成功!".format(filename)
+        self.finish(response_data)
 
 
 def main():
@@ -32,6 +57,7 @@ def main():
         [
             (r"/", MainHandler),
             (r"/upload", UploadHandler),
+            (r"/upload_b", UploadBinaryHandler)
             # (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(current_path, "static")}),
             # (r"/templates/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(current_path, "templates")}),
         ],
