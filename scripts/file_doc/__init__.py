1555b4b416a65581695882915c1323112411dec8
759729997 <qiaoyongtian@qq.com>
2020-06-08 13:44:57 +0800

    0608

--- scripts/file_doc/__init__.py
+++ scripts/file_doc/__init__.py
@@ -7,4 +7,6 @@ pip install python-docx
 doc与docx文件内容读取器,https://python-docx.readthedocs.io/en/latest/.
 https://blog.csdn.net/weixin_42844736/article/details/104513899
 elasticsearch ingest-attachment 对于 word、pdf等文件内容的索引：https://www.jianshu.com/p/774e5ed120ba
+其他库：
+Convert Word documents (.docx files) to HTML：https://github.com/mwilliamson/python-mammoth
 """
