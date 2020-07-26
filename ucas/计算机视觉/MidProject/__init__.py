0cc3b14fc40280f82e7ec4dfbb874e3e7094fd43
qiaoyngtian <759729997@qq.com>
2020-07-26 20:07:29 +0800

    0726

---
+++ ucas/计算机视觉/MidProject/__init__.py
@@ -0,0 +1,7 @@
+"""
+https://segmentation-models.readthedocs.io/en/latest/api.html#unet
+Unet： H and W of input images should be divisible by factor 32.
+Linknet：H and W of input images should be divisible by factor 32.
+FPN：H and W of input images should be divisible by factor 32.
+PSPNet：H and W should be divisible by 6 * downsample_factor and NOT None；downsample_factor=8
+"""
